ecd95dc606de9e1fbea2d74ec0c8daaef74b24fd
Marcin Topczewski <marcintopczewski90@gmail.com>
2022-07-29 19:02:02 +0200

    Change print and str

--- calculator.py
+++ calculator.py
@@ -16,28 +16,26 @@ def division(x, y):
 
 
 if __name__ == '__main__':
-    choice = input('Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ')
-    if choice >= '5':
+    choice = int(input('Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: '))
+    if choice > 4:
         print('Nie wybrałeś poprawnie działania')
     else:
         num1 = float(input('Podaj składnik 1. '))
         num2 = float(input('Podaj składnik 2. '))
-        if choice == '1':
+        if choice == 1:
             logging.debug(f'Dodaję {num1:.2f} i {num2:.2f}')
-            add_result = add(num1, num2)
-            print(f'Wynik to {add_result:.2f}')
+            result = add(num1, num2)
 
-        elif choice == '2':
+        elif choice == 2:
              logging.debug(f'Odejmuję {num1:.2f} i {num2:.2f}')
-             subtraction_result = subtraction(num1, num2)
-             print(f'Wynik to {subtraction_result:.2f}')
+             result = subtraction(num1, num2)
 
-        elif choice == '3':
+        elif choice == 3:
              logging.debug(f'Mnożę {num1:.2f} i {num2:.2f}')
-             multiplication_result = multiplication(num1, num2)
-             print(f'Wynik to {multiplication_result:.2f}')
+             result = multiplication(num1, num2)
 
-        elif choice == '4':
+        elif choice == 4:
              logging.debug(f'Dzielę {num1:.2f} i {num2:.2f}')
-             division_result = division(num1, num2)
-             print(f'Wynik to {division_result:.2f}')
+             result = division(num1, num2)
+        print(f'Wynik to {result:.2f}')
+        
